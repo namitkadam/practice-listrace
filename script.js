152fafecf67ab3a560760d42d2e63348b3f8d891
namitkadam <nk.namitkadam@gmail.com>
2023-11-21 15:00:57 +0530

    practice listrace

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggle() {
+  const navBar = document.querySelector(".nav-bar");
+  if (navBar.style.display === "block") {
+    navBar.style.display = "none";
+    !mportane;
+  } else {
+    navBar.style.display = "block";
+  }
+}
